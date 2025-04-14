17e9b6f2cbaafe9139da8a7ac15c3c5163699dd3
papaparadox <tymurholovko@gmail.com>
2025-04-14 15:49:12 +0100

    Changed the file's contents to be correct

---
+++ data/data.js
@@ -0,0 +1,8 @@
+   const devling1 = {
+                'devling-name': 'Laura',
+                'front-end': Math.random(1, 3),
+                'back-end': Math.random(1, 3),
+                'dev-ego': Math.random(1, 3),
+                'emotional': Math.random(1, 3),
+                'google-skills': Math.random(1, 3)
+            }
